6a481688a4166f1a5e77f4aa3e63b1d3a9a741f0
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-06 19:30:32 +0300

    ex.2 lab had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
 //
+void star_N(int a, int i = 0) {
+	if (i < a) {
+		cout << "*";
+		star_N(a, ++i);
+	}
+	
+
+}
 int main()
 {
-	cout << "Hello world" << endl;
+	int num_user;
+	
+	do
+	{
+		cout << "Enter number: ";
+		cin >> num_user;
+	} while (num_user < 1);
+	star_N(num_user);
+
+	cout << "\nThe end" << endl;
 	system("pause");
 	return 0;
 }
