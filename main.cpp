ce697a09faf6062b5ccdf76e0babb844d6d06fbb
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-06 19:14:48 +0300

    ex.1 lab had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
 //
+int pow(int a, int b, int pow_num = 1, int i = 0) {
+	if (i < b) {
+		pow_num *= a; 
+		pow(a, b, pow_num,++i);
+	}
+	else {
+		return pow_num;
+	}
+	
+}
 int main()
 {
-	cout << "Hello world" << endl;
+	int f_num, s_num;
+	cout << "Enter number: ";
+	cin >> f_num;
+	cout << "Enter pow: ";
+	cin >> s_num;
+	cout << f_num << " ^ " << s_num << " = " << pow(f_num, s_num) << endl;
+	cout << "The end" << endl;
 	system("pause");
 	return 0;
 }
